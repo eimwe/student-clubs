ae14a79eba8fac6ca5b5537f159e1414b6c6d56b
eimwe <prokopenkoserge@inbox.ru>
2023-12-22 12:03:07 +0600

    Added project template

---
+++ src/main/java/com/mycompany/student/clubs/StudentClubs.java
@@ -0,0 +1,12 @@
+package com.mycompany.student.clubs;
+
+/**
+ *
+ * @author eimwe
+ */
+public class StudentClubs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
